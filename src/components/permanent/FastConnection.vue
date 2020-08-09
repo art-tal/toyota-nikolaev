d5b193664fb189fcbced95b8f2e2eba1323de88c
AlexFox2301 <talanov1984>
2020-08-09 22:20:28 +0300

    2020.08.09

--- src/components/permanent/FastConnection.vue
+++ src/components/permanent/FastConnection.vue
@@ -139,7 +139,7 @@
             identifyDevice() {
                 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 
-                    console.log("Вы используете мобильное устройство (телефон или планшет).");
+                    // console.log("Вы используете мобильное устройство (телефон или планшет).");
                     this.viberContext = 'viber://add?number=+';
 
                 } else {
